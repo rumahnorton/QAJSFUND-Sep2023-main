f333b90da3df1ff5ebe6e165f8c335c35cd57f15
Rumah Norton <you@example.com>
2023-11-07 12:25:04 +0000

    added up to step 21

--- 05_JavaScriptArrays/starter/index.js
+++ 05_JavaScriptArrays/starter/index.js
@@ -6,3 +6,24 @@ console.log(quote[2]);
 quote.pop;
 
 quote.push("father");
+
+console.log(quote);
+
+quote.unshift("Luke");
+
+console.log(quote);
+
+let erroneousWord = ["Luke"];
+
+let lukeIsHere = quote.find(n => {
+    return n === erroneousWord
+});
+
+let lukeIsAt;
+if (lukeIsHere) {
+    lukeIsAt = quote.findIndex(n => {
+        return n === erroneousWord
+    });
+    quote[lukeIsAt] = "No";}
+
+    console.log(quote);
