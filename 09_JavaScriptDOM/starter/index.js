03eb15e5c8088dd911d2e9cc1b83717972c5e48b
Rumah Norton <you@example.com>
2023-11-09 09:42:29 +0000

    made a start on more labs

--- 09_JavaScriptDOM/starter/index.js
+++ 09_JavaScriptDOM/starter/index.js
@@ -1,1 +1,3 @@
 const colours = ['red', 'green', 'blue', 'orange', 'lime','olive', 'black', 'yellow'];
+
+const buildP = [``]
--- 10_JavaScriptStyles/starter/index.js
+++ 10_JavaScriptStyles/starter/index.js
@@ -4,7 +4,13 @@ document.querySelector('button#bgColour').addEventListener('click', greenToPink)
 document.querySelector('button#fonts').addEventListener('click', tnrToArial);
 // Do not alter lines of code 2-4!
 
+console.log(blueParagraph.style.color);
+console.log(getComputedStyle(blueParagraph).color);
+
 function blueToRed() {}
+blueParagraph.style.color = "red"; 
+console.log(blueParagraph.style.color);
+console.log(getComputedStyle(blueParagraph).color);
 
 function greenToPink() {}
 
