7a8d2bac3d938497ef6c4a8853d96416219b07a0
Rumah Norton <you@example.com>
2023-11-09 09:53:17 +0000

    made changes

---
+++ 13_JavaScriptModules/starter/circle-functions.js
@@ -0,0 +1,8 @@
+//creating a function area that takes in radius
+// within this function it has pi r squared
+//this can be used elsewhere because it has the export feature
+
+export function area(r){
+    return Math.PI * Math.pow(r,2)
+}
+
